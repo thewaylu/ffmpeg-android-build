1bd5e64ccb24db2a1f615cef562a956f6fde9715
user <user@example.com>
2026-05-25 04:28:22 +0800

    fix: force NDK clang as assembler for x265 ARM NEON .S files

--- build.sh
+++ build.sh
@@ -111,8 +111,9 @@ if [ ! -f x265_done ]; then
         -DCMAKE_ANDROID_API=$API \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX=$PREFIX \
-        -DENABLE_SHARED=OFF -DENABLE_CLI=OFF \
-        -DCMAKE_ANDROID_STANDALONE_TOOLCHAIN=
+        -DCMAKE_ASM_COMPILER=${CC} \
+        -DCMAKE_ASM_FLAGS="--target=${TARGET} -march=armv8-a" \
+        -DENABLE_SHARED=OFF -DENABLE_CLI=OFF
     make $MAKEFLAGS && make install
     # x265 cmake doesn't install headers, do it manually
     cp ../source/x265.h $PREFIX/include/
